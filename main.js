8bf2de9d70594de6d0e0ca3c071db49417e413d5
Logan Murphy <logan.murphy@youvisit.com>
2017-01-31 09:59:58 -0500

    more upgraders

--- main.js
+++ main.js
@@ -266,7 +266,7 @@ module.exports.loop = function () {
                             break;
                         }
                         
-                        switch(storage.length ? creep.memory.id % 3 : 0) {
+                        switch(storage.length ? creep.memory.id % 2 : 0) {
                             case 0 :
                                  if(cs.length && controller.ticksToDowngrade >= 10000) {
                                     var c = target(creep, cs.slice(0));
